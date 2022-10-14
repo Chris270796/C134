65e0adcc13940582da90bf4f553cbf2483c79503
Chris270796 <84269447+Chris270796@users.noreply.github.com>
2022-10-14 16:13:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.size(380, 380);
+    video.hide();
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = " Status : Detecting Objects";
 }
